8bcde388e724949066e37d28f9454d36076cad02
Anushtha Pandit <kumarsen.bash@gmall.com>
2023-07-10 10:59:45 +0530

    token exp handling

--- backend/libs/jwt.js
+++ backend/libs/jwt.js
@@ -1,15 +1,16 @@
 const jwt = require("jsonwebtoken");
+const ErrorResponse = require("../utils/ErrorResponse");
 
 module.exports.generateToken = ({ id, email, role }) => {
 	const privateKey = process.env.TOKEN_KEY;
-	return jwt.sign({ id, email, role }, privateKey, { expiresIn: 60 * 60 * 12 }); //12hr exp
+	return jwt.sign({ id, email, role }, privateKey, { expiresIn: "6h" });
 };
 
 module.exports.verifyToken = (token) => {
 	try {
 		const privateKey = process.env.TOKEN_KEY;
-		return jwt.verify(token, privateKey);
+		jwt.verify(token, privateKey);
 	} catch (err) {
-		return null;
+		throw new ErrorResponse("Authorization failed!", 401);
 	}
 };
